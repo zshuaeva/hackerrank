116ff91de5c52b6acd79cbc2167d28073bd93855
zshuaeva <114031451+zshuaeva@users.noreply.github.com>
2023-07-18 12:58:03 -0700

    Create staircase.py

---
+++ staircase.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'staircase' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+def staircase(n):
+    for i in range(1, n + 1):
+        space = " " * (n - i)
+        pound = "#" * i
+        print(space + pound)
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    staircase(n)
